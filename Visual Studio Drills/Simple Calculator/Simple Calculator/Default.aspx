ad38d73587671195a487e6db7bedc0b83df3200b
Sagebeach <spicywiseman@gmail.com>
2018-01-17 16:21:17 -0800

    asp.net calculator
    a simple c# web form calculator with minor styling

---
+++ Visual Studio Drills/Simple Calculator/Simple Calculator/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Simple_Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .fontstyle {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .resultLabelStyle {
+            background-color: #008080;
+            font-weight: 700;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="fontstyle">
+            <h1>Simple Calculator</h1>
+            <br />
+            First Value: <asp:TextBox ID="value1TextBox" runat="server" Width="90px"></asp:TextBox>
+            <br />
+            Second Value:
+            <asp:TextBox ID="value2TextBox" runat="server" Width="90px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="additionButton" runat="server" OnClick="additionButton_Click" Text="+" />
+&nbsp;<asp:Button ID="subtractionButton" runat="server" Text="-" OnClick="subtractionButton_Click" />
+&nbsp;<asp:Button ID="multiplicationButton" runat="server" Text="*" Width="16px" OnClick="multiplicationButton_Click" />
+&nbsp;<asp:Button ID="divisionButton" runat="server" Text="/" OnClick="divisionButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" CssClass="resultLabelStyle"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Visual Studio Drills/Simple Calculator/Simple Calculator/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Simple_Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            string a = value1TextBox.Text;
+            string b = value2TextBox.Text;
+            int firstVariable = int.Parse(a);
+            int secondVariable = int.Parse(b);
+
+            int sum = firstVariable + secondVariable;
+            string result = sum.ToString();
+            resultLabel.Text = result;
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            string a = value1TextBox.Text;
+            string b = value2TextBox.Text;
+            int firstVariable = int.Parse(a);
+            int secondVariable = int.Parse(b);
+
+            int difference = firstVariable - secondVariable;
+            string result = difference.ToString();
+            resultLabel.Text = result;
+        }
+
+        protected void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            string a = value1TextBox.Text;
+            string b = value2TextBox.Text;
+            int firstVariable = int.Parse(a);
+            int secondVariable = int.Parse(b);
+
+            int product = firstVariable * secondVariable;
+            string result = product.ToString();
+            resultLabel.Text = result;
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            string a = value1TextBox.Text;
+            string b = value2TextBox.Text;
+            double firstVariable = double.Parse(a);
+            double secondVariable = double.Parse(b);
+
+            double quotient = firstVariable / secondVariable;
+            string result = quotient.ToString();
+            resultLabel.Text = result;
+        }
+    }
+}
