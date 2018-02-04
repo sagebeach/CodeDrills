e17843b27c7e21aadad810721f6cf5f51ff8078a
Sage Beach <32965757+sagebeach@users.noreply.github.com>
2018-02-03 21:17:08 -0800

    Code Drill: methods and method parameters.
    At instructor direction I wrote a program to calculate shipping cost based off of some predetermined business rules. The purpose of this exercise was to familiarize myself with methods and their usage in c# code. This was also a valuable learning experience because I had to refactor my original code due to input errors. by doing this I gained valuable insight into how and especially when it is useful to implement method parameters.

---
+++ Visual Studio Drills/ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChallengePostalCalculatorHelperMethods.Startup))]
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
