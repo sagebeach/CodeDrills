62f3d39c393708a95a50959beb4b1231c0297512
Sage Beach <32965757+sagebeach@users.noreply.github.com>
2018-02-08 17:47:00 -0800

    Slot Machine starting
    The first few methods of a randomized slot machine

---
+++ Visual Studio Drills/ChallengeSlotMachine/ChallengeSlotMachine/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="imageReel1" runat="server" Height="150px" Width="150px" />
+            <asp:Image ID="imageReel2" runat="server" Height="150px" Width="150px" />
+            <asp:Image ID="imageReel3" runat="server" Height="150px" Width="150px" />
+            <br />
+            <br />
+            Your Bet: <asp:TextBox ID="userBetTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="leverButton" runat="server" OnClick="leverButton_Click" Text="Pull the Lever!" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="moneyLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            1 Cherry - 2x Your Bet<br />
+            2 Cherry - 3x Your Bet<br />
+            3 Cherry - 4x Your Bet<br />
+            3 7&#39;s - Jackpot! - 100x Your Bet<br />
+            However... if there&#39;s even one BAR you win nothing</div>
+    </form>
+</body>
+</html>
---
+++ Visual Studio Drills/ChallengeSlotMachine/ChallengeSlotMachine/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //apply random image to the reels on button click/
+        //randomize one wheel
+        //randomize all wheels
+    //evaluate image result for the value result
+        //value of cherries, bar, and jackpot
+    // apply value result to money total
+        //caluclate win/loss
+        //make sure results persist to viewstate
+
+
+    protected void leverButton_Click(object sender, EventArgs e)
+    {
+        
+        //string slotImage = randomizeImageResults();
+        string[] imageReel = new string[] {randomizeImageResults(), randomizeImageResults(), randomizeImageResults()}
+        setRandomizedImages(imageReel);    
+        }
+
+    private void setRandomizedImages(string[] imageReel)
+    {
+       
+        imageReel1.ImageUrl = "/images/" + imageReel[0] + ".png";
+        imageReel2.ImageUrl = "/images/" + imageReel[1] + ".png";
+        imageReel3.ImageUrl = "/images/" + imageReel[2] + ".png";
+    }
+
+        Random random = new Random();
+    private string randomizeImageResults()
+    {
+        string[] slotImages = new string[] { "Bar", "Bell", "Cherry", "Clover", "Diamond", "Horseshoe", "Lemon", "Orange", "Plum", "Seven", "Strawberry", "Watermellon" };
+        return slotImages[random.Next(11)];
+    }
+/*
+   private string setReelImage()
+    {
+       imageReel1.ImageUrl = "/images/" + randomizeImageResults() + ".png";
+    } */
+}
